b11be2453e15677f2b223006cd36d83880ff5894
robsongomes <nosbor84@gmail.com>
2020-08-01 09:15:53 -0300

    before migrating to useReducer

--- src/pages/article/index.js
+++ src/pages/article/index.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import { useFetch } from 'hooks/useFetch'
+import TagList from 'pages/components/TagList'
+import Loading from 'pages/components/Loading'
+import DisplayMessage from 'pages/components/DisplayMessage'
 
 const Article = () => {
-  return <div>Article</div>
+  const { slug } = useParams()
+  const URL = `/articles/${slug}`
+  const [{ loading, response, error }, doFetch] = useFetch(URL)
+
+  useEffect(() => {
+    doFetch()
+  }, [doFetch])
+
+  return (
+    <div className="article-page">
+      <div className="banner">
+        {!loading && response && (
+          <div className="container">
+            <h1>{response.article.title}</h1>
+            <div className="article-meta">
+              <Link to={`/profiles/${response.article.author.username}`}>
+                <img src={response.article.author.image} alt="" />
+              </Link>
+              <div className="info">
+                <Link to={`/profiles/${response.article.author.username}`}>
+                  {response.article.author.username}
+                </Link>
+                <span className="date">{response.article.createdAt}</span>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="container page">
+        {loading && <Loading />}
+        {error && <DisplayMessage message="Something bad occurred" />}
+        {!loading && response && (
+          <div className="row article-content">
+            <div>
+              <p>{response.article.body}</p>
+            </div>
+            <TagList tags={response.article.tagList} />
+          </div>
+        )}
+        <hr />
+      </div>
+    </div>
+  )
 }
 
 export default Article
--- src/pages/components/Feed.js
+++ src/pages/components/Feed.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import TagList from 'pages/components/TagList'
 
 const Feed = ({ articles }) => {
   return articles.map((article, index) => (
@@ -18,13 +19,7 @@ const Feed = ({ articles }) => {
           <h1>{article.title}</h1>
           <p>{article.description}</p>
           <span>Read more...</span>
-          <ul className="tag-list">
-            {article.tagList.map((tag) => (
-              <li key={tag} className="tag-default tag pill tag-outline">
-                {tag}
-              </li>
-            ))}
-          </ul>
+          <TagList tags={article.tagList} />
         </Link>
       </div>
     </div>
